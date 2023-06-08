6169f2c4fcf94e548ac278bb7802e0e631714999
Julien L <julienlenaz96@gmail.com>
2023-06-07 22:55:52 -0400

    Created alerts which show subtraciton, division, and multipication results alongside prior addition results

--- js/scripts.js
+++ js/scripts.js
@@ -22,6 +22,9 @@ function multiply(number1, number2) {
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert(("The addition of your answers equals ") + add(number1, number2) + (". The difference between your answers equals ") + subtract(number1, number2) + ("The multiplication of your numbers equals ") + divide(number1, number2) + (". The division between your numbers equals") + divide(number1, number2) + ("."));
+window.alert(
+  ("The addition of your answers equals ") + add(number1, number2) + (". The difference between your answers equals ") + subtract(number1, number2) + 
+  (". The multiplication of your numbers equals ") + multiply(number1, number2) + 
+  (". The division between your numbers equals ") + divide(number1, number2) + ("."));
 
 
