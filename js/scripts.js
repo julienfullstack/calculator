8c2df5213297f009f4617b0817d331df255ff1e8
Julien L <julienlenaz96@gmail.com>
2023-06-07 23:08:18 -0400

    Changed alert message to display function in arabic numberals as opposed to written statements

--- js/scripts.js
+++ js/scripts.js
@@ -21,10 +21,29 @@ function multiply(number1, number2) {
 // user interface logic 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
+const stringNumber1 = number1.toString();
+const stringNumber2 = number2.toString();
 
-window.alert(
-  ("The addition of your answers equals ") + add(number1, number2) + (". The difference between your answers equals ") + subtract(number1, number2) + 
-  (". The multiplication of your numbers equals ") + multiply(number1, number2) + 
-  (". The division between your numbers equals ") + divide(number1, number2) + ("."));
+
+
+
+window.alert( 
+(stringNumber1) + (" + ") + 
+(stringNumber2) + (" = ") + 
+add(number1, number2) + 
+(", ") + 
+(stringNumber1) + (" - ") + 
+(stringNumber2) + (" = ") +
+subtract(number1,number2) + 
+(", ") +
+(stringNumber1) + (" / ") + 
+(stringNumber2) + (" = ") +
+divide(number1, number2) + 
+(", ") +
+(stringNumber1) + (" x ") + 
+(stringNumber2) + (" = ") +
+multiply(number1, number2) +
+(".")
+);
 
 
