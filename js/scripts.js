f784608ae92b7414fa8912744690ac320077f45f
Julien L <julienlenaz96@gmail.com>
2023-06-07 22:12:42 -0400

    distinguished between business and UI logic, removed saySomething function

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,4 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
-saySomething("hi");
-
-const result = add(3, 5);
-window.alert(result);
-
+saySomething("The sum is " + add(3,5) + ".");
