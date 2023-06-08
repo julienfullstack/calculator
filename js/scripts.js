f4a83615625f29397740cecc0c3c1fff76fc9a01
Julien L <julienlenaz96@gmail.com>
2023-06-07 21:59:51 -0400

    Summarized last 3 lines of code in js into one line of code

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function saySomething(whatToSay) {
+  window.alert(whatToSay);
+}
+
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+saySomething("hi");
+
+const result = add(3, 5);
+window.alert(result);
+
